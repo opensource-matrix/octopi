66b9737083b25a8a0d6efe97743d7a1602accbfd
Zack <mcodikid@gmail.com>
2019-08-28 13:24:58 -0600

    &*

---
+++ publish.bat
@@ -0,0 +1,5 @@
+cmd /c node 1up.js
+cmd /c npm publish
+git add *
+git commit -m "&*"
+git push
--- server.js
+++ server.js
@@ -2,29 +2,11 @@
 Localhost your Octopi application.
 */
 
-/* Load modules. */
-var express = require('express'),
-    fs = require('fs'),
-    path = require('path');
-
-/* Initialize PHP support, we'll use this more later. */
-var phpSupport = require('express-php');
-var phpExpress = phpSupport({
-    binPath: 'php'
-});
-
-/* Set up Express to be our server. */
+var express = require('express');
+var php = require('express-php');
 var app = express();
-
-app.set('views', './views');
-app.engine('php', phpExpress.engine);
-app.set('view engine', 'php');
-
-/* Redirect all .PHP requests to phpExpress. */
-app.all(/.+\.php$/, phpExpress.router);
-
-var server = app.listen(8080, (function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Octopi application hosted at http://%s:%s\nMade with ❤️ by Matrix.', host, port);
-}))
+ 
+app.use(php.cgi('./views'));
+app.use(express.static('./views'));
+ 
+app.listen(4000);
