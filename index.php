2052a3ae023f65e2c4589924804eaff60ed330aa
Zack <mcodikid@gmail.com>
2019-08-28 13:55:51 -0600

    Add simple routing.

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo "Hello, world!"
+$request = $_SERVER['REQUEST_URI'];
+
+$debug = False;
+
+if(substr($request, 0, 7) == '/octopi') {
+    $debug = True;
+}
+
+if($debug == True) {
+    echo $request;
+}
 ?>
