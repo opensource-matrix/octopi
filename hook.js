75fb3c66aa8463f40083fa610612577664af41fb
Zack <mcodikid@gmail.com>
2019-08-28 14:32:16 -0600

    Automatic update.

--- hook.js
+++ hook.js
@@ -5,6 +5,7 @@ var chokidar = require('chokidar'),
 
 var watcher = chokidar.watch('.', {ignored: /(pacakge-lock.json)/, persistent: true});
 
+var crypto = require('crypto');
 var current_date = (new Date()).valueOf().toString();
 var random = Math.random().toString();
 crypto.createHash('sha1').update(current_date + random).digest('hex');
