643be7213d5ea38c75f1cd1b6bbe7db9ac3d960f
Zack <mcodikid@gmail.com>
2019-08-28 14:33:52 -0600

    Automatic update.

--- hook.js
+++ hook.js
@@ -12,15 +12,12 @@ var random = Math.random().toString();
 watcher
   .on('add', function(path) {
         child_process.execSync('publish.bat ' + crypto.createHash('sha1').update(current_date + random).digest('hex'));
-        console.log('Found update.');
     })
   .on('change', function(path) {
         child_process.execSync('publish.bat ' + crypto.createHash('sha1').update(current_date + random).digest('hex'));
-        console.log('Found update.');
     })
   .on('unlink', function(path) {
         child_process.execSync('publish.bat ' + crypto.createHash('sha1').update(current_date + random).digest('hex'));
-        console.log('Found update.');
     })
   .on('error', function(path) {
     throw new Error('Error.');
