8fe4ddd81544eb220259c4c1c36335b5b42a38d7
Zack <mcodikid@gmail.com>
2019-08-28 15:01:59 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- routes/route.php
+++ routes/route.php
@@ -3,6 +3,8 @@ class Route {
     public function __construct($path, $controller) {
         if(!isset($path)) {
             throw new Exception();
+        } elseif(!isset($controller))) {
+            
         }
     }
 }
