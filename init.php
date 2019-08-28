f2f3cd93d5bbfd2230ae25d25c6d01c4b735516a
Zack <mcodikid@gmail.com>
2019-08-28 14:38:15 -0600

    Automatic Update 9a6da261d1b72e9beb72e5b6ff642504c0bdb803

---
+++ init.php
@@ -0,0 +1,7 @@
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$response = new Response();
