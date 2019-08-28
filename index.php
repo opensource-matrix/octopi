16fd52a9fbaeba6362426a18f5c279b17a9fe1cb
Zack <mcodikid@gmail.com>
2019-08-28 14:53:02 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
-    $loader = require 'vendor/autoload.php';
-    $loader->register();
+$loader = require 'vendor/autoload.php';
+$loader->register();
 
-    use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
     
-    $request = Request::createFromGlobals();
+$request = Request::createFromGlobals();
     
-    switch($request->getPathInfo()) {
-        case '/':
-            echo 'This is the home page';
-            break;
-        case '/about':
-            echo 'This is the about page';
-            break;   
-        default:
-            echo 'Not found!';
-    }
+switch($request->getPathInfo()) {
+    case '/':
+        echo 'This is the home page';
+        break;
+    case '/about':
+        echo 'This is the about page';
+        break;   
+    default:
+        echo 'Not found!';
+}
