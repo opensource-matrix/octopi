209d1f0ba27117ea4277d3943d1e51f56ccbc306
Zack <mcodikid@gmail.com>
2019-08-28 14:52:39 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
-switch($_SERVER['PATH_INFO']) {
-    case '/':
-        echo 'This is the home page';
-        break;
-    case '/about':
-        echo 'This is the about page';
-        break;   
-    default:
-        echo 'Not found!';
-}
+    $loader = require 'vendor/autoload.php';
+    $loader->register();
+
+    use Symfony\Component\HttpFoundation\Request;
+    
+    $request = Request::createFromGlobals();
+    
+    switch($request->getPathInfo()) {
+        case '/':
+            echo 'This is the home page';
+            break;
+        case '/about':
+            echo 'This is the about page';
+            break;   
+        default:
+            echo 'Not found!';
+    }
