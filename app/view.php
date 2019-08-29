dc40cc565f839532b6fb4bd34f418f78913df716
Zack <mcodikid@gmail.com>
2019-08-29 11:03:06 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

---
+++ app/view.php
@@ -0,0 +1,22 @@
+<?php
+
+function join_paths() {
+    $paths = array();
+
+    foreach (func_get_args() as $arg) {
+        if ($arg !== '') { $paths[] = $arg; }
+    }
+
+    return preg_replace('#/+#', DIRECTORY_SEPARATOR, join(DIRECTORY_SEPARATOR, $paths));
+}
+
+function view($path) {
+    $path = realpath(join_paths(__DIR__, '..', 'views', $path));
+    if(file_exists($path)) {
+        $handle = fopen($path, 'r') or die('Unable to open file!');
+        $data = fread($path, filesize($path, 'r'));
+        echo 'Test';
+        fclose($handle);
+        return $data;
+    }
+}
