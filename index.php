a7603f364927b31c7014f5ddaf4173475e36b378
Zack <mcodikid@gmail.com>
2019-08-29 09:55:23 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -28,6 +28,14 @@ $routes = $Route->getData();
 $gets = $routes['gets'];
 foreach($gets as $route) {
     if($route['path'] === $path) {
+        if(is_callable($route['controller'])) {
+            $func = $route['controller'];
+            $func();
+            $good = True;
+        } elseif(gettype($route['controller']) == 'string') {
+            require_once join_paths('controllers', $route['controller']);
+            $good = True;
+        }
         if(!file_exists(join_paths('controllers', $route['controller']))) {
             echo "Path does not exist.";
         } else {
