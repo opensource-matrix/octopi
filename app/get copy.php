b4bc8f253016fbe16c519f552b9346f437c3181b
Zack <mcodikid@gmail.com>
2019-08-29 10:13:55 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

---
+++ app/get copy.php
@@ -0,0 +1,23 @@
+<?php
+
+function doGet($gets, $path, $response) {
+    $good = False;
+    foreach($gets as $route) {
+        if($route['path'] === $path) {
+            if(is_callable($route['controller'])) {
+                $func = $route['controller'];
+                $response->setContent($func());
+                $good = True;
+            } elseif(gettype($route['controller']) == 'string') {
+                if(!file_exists(join_paths('controllers', $route['controller']))) {
+                    echo "Path does not exist.";
+                } else {
+                    include join_paths('controllers', $route['controller']);
+                    $good = True;
+                }
+            }
+            //$good = True;
+        }
+    }
+    return $good;
+}
