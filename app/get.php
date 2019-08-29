5eb0d9d977a06fd27cd023a4ec0117993a97b79c
Zack <mcodikid@gmail.com>
2019-08-29 10:18:37 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/get.php
+++ app/get.php
@@ -8,13 +8,6 @@ function doGet($gets, $path, $response) {
                 $func = $route['controller'];
                 $response->setContent($func());
                 $good = True;
-            } elseif(gettype($route['controller']) == 'string') {
-                if(!file_exists(join_paths('controllers', $route['controller']))) {
-                    echo "Path does not exist.";
-                } else {
-                    include join_paths('controllers', $route['controller']);
-                    $good = True;
-                }
             }
             //$good = True;
         }
