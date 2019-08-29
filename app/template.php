466ed3b8e0991f0548f254a7e29b50a5403a450d
Zack <mcodikid@gmail.com>
2019-08-29 13:38:39 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/template.php
+++ app/template.php
@@ -7,12 +7,9 @@ built in, and it's not directly used by the user.
 */
 
 function Grill($file, $data) {
-    $template = file_get_contents($file);
-
     foreach($data as $key => $value)
     {
         $template = str_replace('{'.$key.'}', $value, $template);
     }
-
     return $template;
 }
