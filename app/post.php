74270ad7b413f622e28a5ead5eebd45935446d44
Zack <mcodikid@gmail.com>
2019-08-29 10:32:12 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/post.php
+++ app/post.php
@@ -1,10 +1,11 @@
 <?php
 
-function doGet($posts, $path, $response) {
+function doPost($posts, $path, $response) {
     $good = False;
     foreach($posts as $route) {
         if($route['path'] === $path) {
             if(is_callable($route['controller'])) {
+                echo
                 $func = $route['controller'];
                 $response->setContent($func());
                 $good = True;
