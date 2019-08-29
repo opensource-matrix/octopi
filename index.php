d7c28e1da7df6cec4a96ac6dcb0f93de02b49cb5
Zack <mcodikid@gmail.com>
2019-08-29 09:51:35 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ foreach($gets as $route) {
             echo "Path does not exist.";
         } else {
             if(!is_callable($route['controller'])) {
-                
+                $route['controller']();
             } elseif(gettype($route['controller']) == 'string') {
                 require_once join_paths('controllers', $route['controller']);
                 $good = True;
