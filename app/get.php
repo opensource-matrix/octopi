be04577f38157438d7f4287b6324d99d7f84e8fc
Zack <mcodikid@gmail.com>
2019-08-29 14:32:07 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

---
+++ app/get.php
@@ -0,0 +1,36 @@
+<?php
+
+function doGet($gets, $path, $response) {
+    $good = False;
+    $regex = '/';
+    foreach($gets as $route) {
+        $paths = explode('/', $route['path']);
+        $i = 1;
+        print_r($regex . '<br>' . $paths);
+        foreach($paths as $pathseg) {
+            $g = preg_match('/\{([a-zA-Z0-9-_]*)\}/', $pathseg, $matches);
+            if(g) {
+                $regex = $regex . '\/[a-zA-Z0-9-_]';
+            } else {
+                $regex = $regex . $paths[i];
+            }
+            echo "<br>"
+            $i++;
+        }
+        echo $regex;
+        print_r($vars);
+        if($route['path'] === $path) {
+            if(is_callable($route['controller'])) {
+                $func = $route['controller'];
+                $response->setContent($func());
+                $good = True;
+            }
+            //$good = True;
+        }
+    }
+    return $good;
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $good = doGet($gets, $path, $response);
+}
