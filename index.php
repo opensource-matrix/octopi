91480c94811475036fbc4a04d53a0da1323cdf49
Zack <mcodikid@gmail.com>
2019-08-29 10:13:06 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $gets = $routes['gets'];
 
 /* GET Method */
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $good = doGet($gets, $path);
+    $good = doGet($gets, $path, $response);
 }
 
 if(!$good) {
