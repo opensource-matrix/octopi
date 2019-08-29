cda9d7c0c56d059726dbced5984581773386622a
Zack <mcodikid@gmail.com>
2019-08-29 10:59:38 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/view.php
+++ app/view.php
@@ -16,7 +16,6 @@ function view($path) {
         $handle = fopen($path, 'r') or die('Unable to open file!');
         $data = fread($handle, filesize($path, 'r'));
         fclose($handle);
-        echo $data;
         return $data;
     }
 }
