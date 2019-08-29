504752714db9d624d6909faf00cce2ac1768479f
Zack <mcodikid@gmail.com>
2019-08-29 09:38:56 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -26,7 +26,6 @@ function join_paths() {
 $routes = $Route->getData();
 $gets = $routes['gets'];
 foreach($gets as $route) {
-    echo $route;
     if($route['path'] === $path) {
         if(!file_exists(join_paths('controllers', $route['controller']))) {
             echo "Path does not exist.";
