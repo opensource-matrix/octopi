9bbb9447d7b2d1ebdf4922cfea3dcbf61ee63f40
Zack <mcodikid@gmail.com>
2019-08-29 14:47:40 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/get.php
+++ app/get.php
@@ -7,7 +7,7 @@ function doGet($gets, $path, $response) {
         $paths = explode('/', $route['path']);
         $i = 1;
         echo $regex . '<br>';
-        foreach($paths as $pathseg) {
+        for($i=0 as $pathseg) {
             $g = preg_match('/\{([a-zA-Z0-9-_]*)\}/', $pathseg, $matches);
             echo $g;
             if(g == 1) {
