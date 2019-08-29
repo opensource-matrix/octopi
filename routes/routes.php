2b347906fe9c7c376144b709cde833d12a315357
Zack <mcodikid@gmail.com>
2019-08-29 11:13:48 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- routes/routes.php
+++ routes/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 $Route::get('/', function() {
-    if(_GET['test'] == 'true') {
+    if($_GET['test'] == 'true') {
         return view('test');
     } else {
         return view('index');
