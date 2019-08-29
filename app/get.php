4b6957534afbf984202e4366d94b4e03c089a00b
Zack <mcodikid@gmail.com>
2019-08-29 13:56:57 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/get.php
+++ app/get.php
@@ -2,7 +2,6 @@
 
 function doGet($gets, $path, $response) {
     $good = False;
-    $paths = explode('/', $path);
     print_r($paths);
     foreach($gets as $route) {
         if($route['path'] === $path) {
