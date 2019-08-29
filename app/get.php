1273736ea083edd53a03cf901992a7e81eb48673
Zack <mcodikid@gmail.com>
2019-08-29 13:47:11 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/get.php
+++ app/get.php
@@ -3,7 +3,7 @@
 function doGet($gets, $path, $response) {
     $good = False;
     $parsed = parse_url($path);
-    echo $parsed;
+    print_r($parsed);
     foreach($gets as $route) {
         if($route['path'] === $path) {
             if(is_callable($route['controller'])) {
