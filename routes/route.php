a09fe186562f5361148d8f1d23e5c4082f0bddc2
Zack <mcodikid@gmail.com>
2019-08-28 21:57:36 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- routes/route.php
+++ routes/route.php
@@ -26,9 +26,7 @@ class octopi_routes {
 
     public static function get($path, $controller) {
         print_r($this->gets);
-        $this->gets(
-            'path' => $path,
-            'controller' => $controller
+        $this->gets[$path] = $controller;
         ));
     }
 
