bc73d62f2c70e6f7defae02343220e158a32f70d
Zack <mcodikid@gmail.com>
2019-08-28 22:03:38 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- routes/route.php
+++ routes/route.php
@@ -21,7 +21,7 @@ class route {
 
 class octopi_routes {
     public function __construct() {
-        $this->gets = array();
+        $this->gets = new ArrayObject();
         echo "Hello world from __construct <br>";
         print_r($this->gets);
     }
