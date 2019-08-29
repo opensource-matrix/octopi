3276dbe8602d9bb7f36410f4520048a049927a11
Zack <mcodikid@gmail.com>
2019-08-29 10:54:54 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/view.php
+++ app/view.php
@@ -13,7 +13,7 @@ function join_paths() {
 function view($path) {
     echo realpath(join_paths(__DIR__, '..', 'views', $path));
     if(file_exists(join_paths(__DIR__, '..', 'views', $path))) {
-        $fn = fopen(join_paths(__DIR__, 'views', '..', $path), 'r') or die('Unable to open file!');
+        $fn = fopen(join_paths(__DIR__, '..', 'views', $path), 'r') or die('Unable to open file!');
         return fread($fn, filesize(join_paths(__DIR__, 'views', '..', $path), 'r'));
     } else {
         echo 'TEST';
