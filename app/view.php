4abede9ea2df4a73db77a1dbc9f10b280d22495b
Zack <mcodikid@gmail.com>
2019-08-29 10:50:33 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/view.php
+++ app/view.php
@@ -14,7 +14,7 @@ function view($path) {
     if(file_exists(join_paths(__DIR__, 'views', '$path'))) {
         $fn = fopen(join_paths(__DIR__, 'views', '$path'), "r");
         $result = null;
-        $fn = fopen(join_paths(__DIR__, 'views', '$path'), 'r') or die("Unable to open file!");
+        $fn = fopen(join_paths(__DIR__, 'views', '$path'), 'r') or die('Unable to open file!');
         return $result;
     } else {
         echo 'TEST';
