9f79760d5ec8fda25d9009fffd8e23c136a1c65a
Zack <mcodikid@gmail.com>
2019-08-29 09:53:53 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,6 @@
 <?php
 
 $Route::get('/', function() {
-    echo "Hello, world!  This is slightly different "
+    echo "Hello, world!  This is slightly different than the /home page, but it'll do.,"
 });
 $Route::get('/home', 'index.php');
