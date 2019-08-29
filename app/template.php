cd6a6f477fdb220251621186e96f86b1762f2399
Zack <mcodikid@gmail.com>
2019-08-29 13:38:09 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/template.php
+++ app/template.php
@@ -6,8 +6,7 @@ Grill is Octopi's templating engine for models & views.  It's
 built in, and it's not directly used by the user.
 */
 
-function Grill($file, $data)
-{
+function Grill($file, $data) {
    $template = file_get_contents($file);
 
    foreach($data as $key => $value)
