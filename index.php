0d72b9133ec517fa4be2258fbf731b405b614f24
Zack <mcodikid@gmail.com>
2019-08-29 09:57:56 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- index.php
+++ index.php
@@ -29,23 +29,8 @@ $routes = $Route->getData();
 $gets = $routes['gets'];
 
 /* GET Method */
-foreach($gets as $route) {
-    if($route['path'] === $path) {
-        if(is_callable($route['controller'])) {
-            $func = $route['controller'];
-            $func();
-            $good = True;
-        } elseif(gettype($route['controller']) == 'string') {
-            if(!file_exists(join_paths('controllers', $route['controller']))) {
-                echo "Path does not exist.";
-            } else {
-                require_once join_paths('controllers', $route['controller']);
-                $good = True;
-                echo "Test";
-            }
-        }
-        //$good = True;
-    }
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    doGet();
 }
 
 if(!$good) {
