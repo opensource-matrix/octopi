347114b067f4bd795c9396f8ddbd48e50474b6fe
Zack <mcodikid@gmail.com>
2019-08-29 10:19:40 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/get.php
+++ app/get.php
@@ -4,7 +4,6 @@ function doGet($gets, $path, $response) {
     $good = False;
     foreach($gets as $route) {
         if($route['path'] === $path) {
-            echo "Test";
             if(is_callable($route['controller'])) {
                 $func = $route['controller'];
                 $response->setContent($func());
