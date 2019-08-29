c19f6459525d9d7c25b5bb92ad156c0e3d9191db
Zack <mcodikid@gmail.com>
2019-08-28 21:39:57 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- routes/route.php
+++ routes/route.php
@@ -34,8 +34,7 @@ class octopi_routes {
 
     function getData() {
         return array(
-            path => $this->path,
-            controller => $this->controller
-        );
+            
+        )
     }
 }
