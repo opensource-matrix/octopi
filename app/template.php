a5cb0e335a5ce52ea25d5fa241c6c453b5e94dd1
Zack <mcodikid@gmail.com>
2019-08-29 13:39:15 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/template.php
+++ app/template.php
@@ -6,8 +6,9 @@ Grill is Octopi's templating engine for models & views.  It's
 built in, and it's not directly used by the user.
 */
 
-function Grill($file, $data) {
-    if(is_subclass_of($data, 'Model')) {
+function Grill($file, $model) {
+    if(is_subclass_of($model, 'Model')) {
+        $d
         foreach($data as $key => $value)
         {
             $template = str_replace('{'.$key.'}', $value, $template);
