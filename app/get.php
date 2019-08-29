4cd2409a0080d416132537ed953be4d77799cca9
Zack <mcodikid@gmail.com>
2019-08-29 09:57:29 -0600

    Automatic Update b786707336910c2dc188d566ec36626ba872681a

--- app/get.php
+++ app/get.php
@@ -1,20 +1,22 @@
 <?php
 
-foreach($gets as $route) {
-    if($route['path'] === $path) {
-        if(is_callable($route['controller'])) {
-            $func = $route['controller'];
-            $func();
-            $good = True;
-        } elseif(gettype($route['controller']) == 'string') {
-            if(!file_exists(join_paths('controllers', $route['controller']))) {
-                echo "Path does not exist.";
-            } else {
-                require_once join_paths('controllers', $route['controller']);
+function doGet() {
+    foreach($gets as $route) {
+        if($route['path'] === $path) {
+            if(is_callable($route['controller'])) {
+                $func = $route['controller'];
+                $func();
                 $good = True;
-                echo "Test";
+            } elseif(gettype($route['controller']) == 'string') {
+                if(!file_exists(join_paths('controllers', $route['controller']))) {
+                    echo "Path does not exist.";
+                } else {
+                    require_once join_paths('controllers', $route['controller']);
+                    $good = True;
+                    echo "Test";
+                }
             }
+            //$good = True;
         }
-        //$good = True;
     }
 }
