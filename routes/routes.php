2b736ad182f843541fe74c264feea1fadece223e
Zack <mcodikid@gmail.com>
2019-08-30 13:32:14 -0600

    Automatic Update 9c52c6f336773c3cb3d2617ff9541e8da456584f

--- routes/routes.php
+++ routes/routes.php
@@ -7,7 +7,9 @@ class DataModel extends Model {
 class DataModel2 extends Model {
     public $websiteName = 'My Octopi Website';
 
-    public function __construct()
+    public function __construct($user) {
+        
+    }
 }
 
 $Route::get('/user', function() {
