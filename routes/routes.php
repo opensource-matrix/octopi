8dfcd1ece5dba9e910babaf2e6c8a383569a6936
Zack <mcodikid@gmail.com>
2019-08-30 13:34:54 -0600

    Automatic Update 9c52c6f336773c3cb3d2617ff9541e8da456584f

--- routes/routes.php
+++ routes/routes.php
@@ -2,7 +2,7 @@
 
 class DataModel extends Model {
     public $websiteName = 'My Octopi Website';
-    public $description = 'This website was made as an example for Octopi.  No username was provided!  Go to /';
+    public $description = 'This website was made as an example for Octopi.  No username was provided!  Go to /user/{name}';
 }
 
 class DataModel2 extends Model {
