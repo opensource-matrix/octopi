899a7133f26713be088e5cc5d6a3c2bebfe338d2
Zack <mcodikid@gmail.com>
2019-08-30 13:11:37 -0600

    Automatic Update 9c52c6f336773c3cb3d2617ff9541e8da456584f

--- app/get.php
+++ app/get.php
@@ -9,7 +9,7 @@ function doGet($gets, $path, $response) {
         if(preg_match($regex, $path) == 1) {
             if(is_callable($route['controller'])) {
                 $func = $route['controller'];
-                $response->setContent($func());
+                $response->setContent(call_user_func_array($func()));
                 $good = True;
             }
             //$good = True;
