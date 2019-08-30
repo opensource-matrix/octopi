5a800720903d8945dc93036fb03095c495778f5a
Zack <mcodikid@gmail.com>
2019-08-30 13:23:11 -0600

    Automatic Update 9c52c6f336773c3cb3d2617ff9541e8da456584f

--- app/view.php
+++ app/view.php
@@ -21,7 +21,7 @@ function view($path, $model) {
         fclose($handle);
         */
         $data = file_get_contents($p);
-        if(file_exists())
+        if(file_exists(realpath(join_paths(__DIR__, '..', 'model', $path))))
         $data = Grill($data, $model);
         return $data;
     } else {
