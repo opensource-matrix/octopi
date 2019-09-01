fb4f6d2ba0e802af2a6d65f8fd83ee2ed2e5d638
Zack <mcodikid@gmail.com>
2019-09-01 13:38:27 -0600

    Automatic Update 20c89a29a8606e107e3df0cc228687e5db04d3b7

--- server.php
+++ server.php
@@ -1,6 +1,16 @@
 <?php
 
 /*
+Symfony Modules
+---------------
+The Symfony modules are used server side for HTTP requests.
+They give Octopi data that powers controllers, routes, soon
+models and views as well. 
+*/
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/*
 | Load the INDEX.PHP File
 |---------------------------------
 | Load the INDEX.PHP File so it can redirect everything.
