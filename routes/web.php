05e8e3aa8cb5b598bd8a2125fcccb8a3e0a02c63
Zack <mcodikid@gmail.com>
2019-09-01 13:57:39 -0600

    Automatic Update 20c89a29a8606e107e3df0cc228687e5db04d3b7

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ $Route::get('/', function() {
 
 $Route::get('/user/:name', function($name) {
     $str = '<h1>' . $name . '</h1>';
-    echo $name;
     if($name == 'Octopi') {
         $str = $str + '<h3>[ADMIN]</h3>';
     }
