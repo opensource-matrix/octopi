7bcf0382474a26d079425a26374b33827d5365d2
Zack <mcodikid@gmail.com>
2019-09-01 13:42:03 -0600

    Automatic Update 20c89a29a8606e107e3df0cc228687e5db04d3b7

--- server.php
+++ server.php
@@ -10,9 +10,71 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+<?php
+
 /*
-| Load the INDEX.PHP File
-|---------------------------------
-| Load the INDEX.PHP File so it can redirect everything.
+Loader
+------
+Loads the autoloader provided with Composer.
 */
-include 'public/index.php';
+$loader = require '../vendor/autoload.php';
+$loader->register();
+
+/*
+Toolkit Modules
+---------------
+Load the Tools that the user can use.
+*/
+include '../app/model.php';
+include '../app/template.php';
+include '../app/view.php';
+
+/*
+Route Modules
+-------------
+Load the Routes to be used later one.
+*/
+include '../routes/route.php';
+include '../routes/web.php';
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+$path = $request->getPathInfo();
+
+/*
+Get Routes
+----------
+This allows us to use the routes in routes/routes.php
+*/
+$routes = $Route->getData();
+$gets = $routes['gets'];
+$posts = $routes['posts'];
+
+
+/*
+Kernel Modules
+--------------
+The Kernel modules power both ends of your application.  They
+are utilized in this very script.
+*/
+include '../app/get.php';
+include '../app/post.php';
+
+/*
+Good Checking
+-------------
+If good is false, that means that the requested page is not found.
+*/
+if(!$good) {
+    $response->setContent('Not found.');
+    $response->setStatus(404);
+}
+
+/*
+Send the Response
+-----------------
+This sends the response to the client, whether it's a web browser
+or an HTTP request.
+*/
+$response->send();
