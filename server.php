0173c8c56cd3e50c268fba3752246d7c8c19e2fd
Zack <mcodikid@gmail.com>
2019-09-01 13:39:27 -0600

    Automatic Update 20c89a29a8606e107e3df0cc228687e5db04d3b7

--- server.php
+++ server.php
@@ -4,7 +4,7 @@
 | Symfony Modules
 |---------------------------------
 | The Symfony modules are used server side for HTTP requests.
-They give Octopi data that powers controllers, routes, soon
+| They give Octopi data that powers controllers, routes, soon
 models and views as well. 
 */
 use Symfony\Component\HttpFoundation\Request;
